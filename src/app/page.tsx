af55da6fe03325faa520e7b17f82dccf7b16352f
Juan <juan21.naranjo@gmail.com>
2023-07-05 14:23:29 +0200

    feat: adds homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,44 @@
+'use client';
+
 import '../styles/main.scss';
 import React, { useState } from 'react';
 import { Container, Row, Col } from 'reactstrap';
+import Navbar from '@/components/Navbar';
+import { Button } from '@/components/Button';
+import Footer from '@/components/Footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-export default function Home() {
+export default function HomePage() {
+  const [hovered, setHovered] = useState(false);
+
+  const hoveredButton = () => {
+    setHovered(true);
+  }
   return (
-    <main>Hello World
+    <main className='App'>
+      <Navbar />
+      <Container>
+        <Row>
+          <Col xs='12' className="mt-xl-3 pt-xl-3 mt-5 pt-5">
+            <p className="header-text text-center fw-bold mb-0">
+              EASY<span className="text-green">TENIS</span>
+            </p>
+            <p className="body-text text-center">#1 TENNIS APP IN SPAIN!</p>
+          </Col>
+          <Col className="button-section">
+            <Button buttonStyle="btn--outline" buttonSize="btn--large" type='button' onClick={hoveredButton}>
+                BOOK COURTS
+            </Button>
+            <Button buttonStyle="btn--outline" buttonSize="btn--large" type='button' onClick={hoveredButton}>
+                TAKE CLASSES
+            </Button>
+            <Button buttonStyle="btn--primary" buttonSize="btn--large" type='button' onClick={hoveredButton}>
+                JOIN MATCHES
+            </Button>
+          </Col>
+        </Row>
+      </Container>
+      <Footer/>
     </main>
   )
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,6 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
 import { Button } from "./Button";
-import "./Navbar.css";
-import { LoginForm } from "./LoginForm";
+import "./Navbar.scss";
 
 function Navbar() {
   const [modalOpen, setModalOpen] = useState(false);
@@ -17,14 +15,14 @@ function Navbar() {
           onClick={showModal}
           buttonStyle="btn--outline"
           buttonSize="btn--medium"
+          type="button"
         >
           Sign up
         </Button>
 
-        <Button buttonStyle="btn--outline" buttonSize="btn--medium">
+        <Button onClick={showModal} buttonStyle="btn--outline" buttonSize="btn--medium" type='button'>
           Log in
         </Button>
-        <LoginForm modalOpen={modalOpen} setModalOpen={setModalOpen}/>
       </nav>
     </>
   );
