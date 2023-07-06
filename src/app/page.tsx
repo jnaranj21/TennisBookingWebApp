41bb1a0c8d0f43ce326395bd3f10e1ac50cce0e2
Sean Dorr <seanmichael.dorr@gmail.com>
2023-07-06 21:53:52 +0200

    fix: modifies btn component logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,44 +1,40 @@
-'use client';
+"use client";
 
-import '../styles/main.scss';
-import React, { useState } from 'react';
-import { Container, Row, Col } from 'reactstrap';
-import Navbar from '@/components/Navbar';
-import { Button } from '@/components/Button';
-import Footer from '@/components/Footer';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "../styles/main.scss";
+import React, { useState } from "react";
+import { Container, Row, Col } from "reactstrap";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Button from "@/components/Button";
 
 export default function HomePage() {
   const [hovered, setHovered] = useState(false);
 
   const hoveredButton = () => {
     setHovered(true);
-  }
+  };
   return (
-    <main className='App'>
+    <main className="App">
       <Navbar />
       <Container>
         <Row>
-          <Col xs='12' className="mt-xl-3 pt-xl-3 mt-5 pt-5">
+          <Col xs="12" className="mt-xl-3 pt-xl-3 mt-5 pt-5">
             <p className="header-text text-center fw-bold mb-0">
               EASY<span className="text-green">TENIS</span>
             </p>
             <p className="body-text text-center">#1 TENNIS APP IN SPAIN!</p>
           </Col>
           <Col className="button-section">
-            <Button buttonStyle="btn--outline" buttonSize="btn--large" type='button' onClick={hoveredButton}>
-                BOOK COURTS
+            <Button style="dashed" onClick={hoveredButton}>
+              BOOK COURTS
             </Button>
-            <Button buttonStyle="btn--outline" buttonSize="btn--large" type='button' onClick={hoveredButton}>
-                TAKE CLASSES
-            </Button>
-            <Button buttonStyle="btn--primary" buttonSize="btn--large" type='button' onClick={hoveredButton}>
-                JOIN MATCHES
-            </Button>
+            <Button onClick={hoveredButton}>TAKE CLASSES</Button>
+            <Button onClick={hoveredButton}>JOIN MATCHES</Button>
           </Col>
         </Row>
       </Container>
-      <Footer/>
+      <Footer />
     </main>
-  )
+  );
 }
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import { Container, Row, Col } from "reactstrap";
-import { Button } from "../components/Button";
-import { Link } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import Link from "next/link";
+import Button from "@/components/Button";
 
 const HomePage = () => {
   const [hovered, setHovered] = useState(false);
@@ -12,16 +12,16 @@ const HomePage = () => {
       <Navbar />
       <Container className="container-customized">
         <Row>
-          <Col xs='12' className="mt-xl-3 pt-xl-3 mt-5 pt-5">
+          <Col xs="12" className="mt-xl-3 pt-xl-3 mt-5 pt-5">
             <p className="header-text text-center fw-bold mb-0">
               EASY<span className="text-green">TENIS</span>
             </p>
             <p className="body-text text-center">#1 TENNIS APP IN SPAIN!</p>
           </Col>
           <Col className="text-center">
-            <Button buttonStyle="btn--outline" buttonSize="btn--large">
+            <Button onClick={() => console.log("Clicked")}>
               <Link
-                to="/directory"
+                href="/directory"
                 style={{
                   textDecoration: "none",
                   color: hovered ? "black" : "#fff",
@@ -32,12 +32,12 @@ const HomePage = () => {
                 BOOK COURTS
               </Link>
             </Button>
-            <Button buttonStyle="btn--outline" buttonSize="btn--large">
+            {/* <Button buttonStyle="btn--outline" buttonSize="btn--large">
               TAKE CLASSES
             </Button>
             <Button buttonStyle="btn--primary" buttonSize="btn--large">
               JOIN MATCHES
-            </Button>
+            </Button> */}
           </Col>
         </Row>
       </Container>
